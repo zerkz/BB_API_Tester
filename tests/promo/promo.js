4ee3585424e642eb474cd59f95a44f074fc606dd
John <john.hofrichter@brandingbrand.com>
2014-01-26 15:51:39 -0500

    refactoring largely complete

---
+++ tests/promo/promo.js
@@ -0,0 +1,51 @@
+var helpers    = require(process.cwd() + '/lib/helpers')
+  , controller = require(process.cwd() + '/lib/controller')
+  , logger     = require(process.cwd() + '/lib/logger')
+  , tests      = require(process.cwd() + '/tests')()
+  , utils      = require(process.cwd() + '/lib/testUtilities')
+
+////// request setup //////
+
+var testClass = 'promo';
+  
+// load config values
+var config = utils.loadJson(__dirname)
+  , forms  = config.requiredForms
+  
+////// exports //////  
+
+module.exports = {
+  fullTest : fullTest,
+  
+  //individual
+  apply   : apply
+}
+
+////// full test set //////
+  
+function fullTest () {
+  return [
+    apply,
+    tests.cart.show
+  ];
+}
+
+////// individual tests //////
+
+function apply () {
+  return {
+    name          : testClass + '.apply',
+    cartDependant : true,
+    exec          : function(error, response, body, callback) {
+      if (!(forms && forms.promo)) {
+        return controller.testFailed(apply.name, 'Failed to parse a promo form', callback);
+      }
+      
+      controller.reqAndLog(apply.name, {
+        uri    : '/checkout/promo',
+        method : 'POST',
+        form   : forms.promo
+      }, callback);
+    }
+  }
+}
