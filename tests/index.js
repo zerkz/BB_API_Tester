3022ca664d671404ccca607fc500c22fbdc48e68
John <john.hofrichter@brandingbrand.com>
2014-02-24 12:29:43 -0500

    added dynamically required test support

--- tests/index.js
+++ tests/index.js
@@ -1,19 +1,16 @@
-module.exports = exports = function () {
-  return {
-    addresses         : require(__dirname + '/addresses/addresses'),
-    cart              : require(__dirname + '/cart/cart'),
-    categories        : require(__dirname + '/categories/categories'),
-    checkout          : require(__dirname + '/checkout/checkout'),
-    control_utilities : require(__dirname + '/control_utilities/utilities'),
-    custom_sets       : require(__dirname + '/custom_sets/custom_sets'),
-    giftcard          : require(__dirname + '/giftcard/giftcard'),
-    products          : require(__dirname + '/products/products'),
-    session           : require(__dirname + '/session/session'),
-    checkout          : require(__dirname + '/checkout/checkout'),
-    promo             : require(__dirname + '/promo/promo'),
-    orders            : require(__dirname + '/orders/orders'),
-    wishlist          : require(__dirname + '/wishlist/wishlist'),
-    shipping_methods  : require(__dirname + '/shipping_methods/shipping_methods'),
-    paypal            : require(__dirname + '/paypal/paypal') 
-  };
+var _ = require('lodash');
+
+module.exports = exports = function () {  
+  var tests = {};
+  
+  _.each(require('fs').readdirSync(__dirname), function (name) {
+    // ignore files
+    if (!/\./.test(name)) {
+      
+      // get the controller for the test, and pass it to the callback
+      tests[name] = require(__dirname + '/' + name + '/controller');
+    }
+  });
+  
+  return tests; 
 }
